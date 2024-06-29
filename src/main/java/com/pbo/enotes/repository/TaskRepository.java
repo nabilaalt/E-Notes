d5c68c3577757c7633c3d6328d3408357a8bbec8
Nabila Lailatanzila <nabila5des@gmail.com>
2024-06-29 17:07:33 +0700

    notes done

--- src/main/java/com/pbo/enotes/repository/TaskRepository.java
+++ src/main/java/com/pbo/enotes/repository/TaskRepository.java
@@ -11,11 +11,13 @@ import org.springframework.data.repository.query.Param;
 import com.pbo.enotes.entity.Task;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
-    List<Task> findByUserId(Long userId);  // Berdasarkan userId
-    List<Task> findByUserIdAndCompleted(Long userId, boolean completed);  // Berdasarkan userId dan status completed
+    List<Task> findByUserId(Long userId); // Berdasarkan userId
+
+    List<Task> findByUserIdAndCompleted(Long userId, boolean completed); // Berdasarkan userId dan status completed
+
     List<Task> findByUserIdAndDueDateAfter(Long userId, Date currentDate);
 
-    
     @Query("SELECT t FROM Task t WHERE t.user.id = :userId AND DATE(t.dueDate) = CURRENT_DATE")
-    List<Task> findByUserIdAndDueDateToday(@Param("userId") Long userId);  // Berdasarkan userId dan tanggal dueDate hari ini
+    List<Task> findByUserIdAndDueDateToday(@Param("userId") Long userId); // Berdasarkan userId dan tanggal dueDate hari
+                                                                          // ini
 }
